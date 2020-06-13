dd5d10e65e8a21eb43d29f80d89aa71709720572
Marcelo Valderrama Correa <marcelo.correa@mjv.com.br>
2020-06-12 21:58:36 -0300

    Nova Aula

--- xml-java/src/br/com/alura/teste/Sistema.java
+++ xml-java/src/br/com/alura/teste/Sistema.java
@@ -5,6 +5,11 @@ import java.io.IOException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -15,7 +20,7 @@ import br.com.alura.model.Produto;
 
 public class Sistema {
 	
-	public static void main(String[] args) throws SAXException, IOException, ParserConfigurationException {
+	public static void main(String[] args) throws SAXException, IOException, ParserConfigurationException, XPathExpressionException {
 		DocumentBuilderFactory fabrica = DocumentBuilderFactory.newInstance();
 		fabrica.setValidating(true);
 		fabrica.setNamespaceAware(true);
@@ -26,7 +31,12 @@ public class Sistema {
 		Element venda = parse.getDocumentElement();
 		String moeda = venda.getAttribute("moeda");
 		System.out.println(moeda);
-		NodeList produtos = parse.getElementsByTagName("produto");
+		String exp = "/venda/produtos/produto[contains(nome,'Livro')]";
+		
+		XPath path = XPathFactory.newInstance().newXPath();
+		
+		XPathExpression expression = path.compile(exp);
+		NodeList produtos = (NodeList) expression.evaluate(parse, XPathConstants.NODESET);
 		
 		for (int i = 0; i < produtos.getLength(); i++) {
 			Element produto = (Element) produtos.item(i);
